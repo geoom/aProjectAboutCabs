429394e4674dd1e9582a94bba18e7ca370ae762d
George Mejia <hi.geoom@gmail.com>
2019-02-12 16:49:56 -0500

    add init structure

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', "~> 2.0.0", require: 'sinatra/base'
+gem 'sinatra-contrib', require: 'sinatra/reloader'
---
+++ app.rb
@@ -0,0 +1,11 @@
+class CabsApp < Sinatra::Base
+  
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+    get '/' do
+        erb :index
+    end
+
+end
